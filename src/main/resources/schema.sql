3b74022a694f6a27b1285544a1cd298189d90397
Alexandr <smoke_91@list.ru>
2016-10-16 22:30:58 +0300

    add schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,11 @@
+CREATE SCHEMA technopark DEFAULT CHARACTER SET utf8 ;
 CREATE TABLE technopark.User
 (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  email VARCHAR(50) CHARACTER SET 'utf8' NOT NULL,
-  name VARCHAR(50) CHARACTER SET 'utf8',
-  username VARCHAR(50) CHARACTER SET 'utf8',
-  about VARCHAR(50) CHARACTER SET 'utf8',
+  email VARCHAR(50) NOT NULL,
+  name VARCHAR(50),
+  username VARCHAR(50),
+  about VARCHAR(50),
   isAnonymous BOOLEAN
 );
 CREATE UNIQUE INDEX User_email_uindex ON technopark.User (email);
@@ -12,9 +13,9 @@ CREATE UNIQUE INDEX User_email_uindex ON technopark.User (email);
 CREATE TABLE technopark.Form
 (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(50) CHARACTER SET 'utf8' NOT NULL,
-  short_name VARCHAR(50) CHARACTER SET 'utf8' NOT NULL,
-  user VARCHAR(50) CHARACTER SET 'utf8' NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  short_name VARCHAR(50) NOT NULL,
+  user VARCHAR(50) NOT NULL,
   CONSTRAINT Forum_User_email_fk FOREIGN KEY (user) REFERENCES User (email)
 );
 CREATE UNIQUE INDEX Forum_name_uindex ON technopark.Forum (name);
@@ -25,10 +26,10 @@ CREATE TABLE technopark.Post
   id INT PRIMARY KEY AUTO_INCREMENT,
   date DATETIME,
   thread INT,
-  message TEXT CHARACTER SET 'utf8',
-  user VARCHAR(50) CHARACTER SET 'utf8' NOT NULL,
+  message TEXT,
+  user VARCHAR(50) NOT NULL,
   CONSTRAINT Post_User_email_fk FOREIGN KEY (user) REFERENCES User (email),
-  forum VARCHAR(50) CHARACTER SET 'utf8' NOT NULL,
+  forum VARCHAR(50) NOT NULL,
   CONSTRAINT Post_Forum_short_name_fk FOREIGN KEY (forum) REFERENCES Forum (short_name),
 
   parent INT,
@@ -42,15 +43,15 @@ CREATE TABLE technopark.Post
 CREATE TABLE technopark.Thread
 (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  forum VARCHAR(50) CHARACTER SET 'utf8' NOT NULL,
+  forum VARCHAR(50) NOT NULL,
   CONSTRAINT Thread_Forum_short_name_fk FOREIGN KEY (forum) REFERENCES Forum (short_name),
-  title VARCHAR(50) CHARACTER SET 'utf8',
+  title VARCHAR(50),
   isClosed BOOLEAN,
-  user VARCHAR(50) CHARACTER SET 'utf8' NOT NULL,
+  user VARCHAR(50) NOT NULL,
   CONSTRAINT Thread_User_email_fk FOREIGN KEY (user) REFERENCES User (email),
   date DATETIME,
-  message TEXT CHARACTER SET 'utf8',
-  slug VARCHAR(50) CHARACTER SET 'utf8',
+  message TEXT,
+  slug VARCHAR(50),
 
   isDelete BOOLEAN
 )
