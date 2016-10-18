3425e2d99f9dd0710e86b9fa6db7f8b439b91f61
alexandr <smoke_91@list.ru>
2016-10-18 23:51:35 +0300

    add user/details

--- src/main/java/org/ebitbucket/main/UserController.java
+++ src/main/java/org/ebitbucket/main/UserController.java
@@ -2,14 +2,18 @@ package org.ebitbucket.main;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.ObjectIdGenerator;
 import org.ebitbucket.services.User;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 final public class UserController {
     private final User user;
-    private StringBuilder userCreate;
 
     public UserController(User user) {
         this.user = user;
@@ -17,7 +21,7 @@ final public class UserController {
     }
 
     @RequestMapping(path="db/api/user/create/", method = RequestMethod.POST)
-    public Result<?> userCreate(@RequestBody CreateRequest body){
+    public Result<?> userCreate(@RequestBody UserProfil body){
         if (StringUtils.isEmpty(body.getEmail())) return Result.invalidReques();
 
         final Integer id = user.create( body.getEmail(),
@@ -34,26 +38,21 @@ final public class UserController {
 
    @RequestMapping(path = "db/api/user/details/?user", method = RequestMethod.GET)
    public Result<?> userDetails(@RequestParam String email){
-       return Result.ok("{\n" +
-               "    \"code\": 0,\n" +
-               "    \"response\": {\n" +
-               "        \"about\": \"hello im user1\",\n" +
-               "        \"email\": \"example@mail.ru\",\n" +
-               "        \"followers\": [\n" +
-               "            \"example3@mail.ru\"\n" +
-               "        ],\n" +
-               "        \"following\": [\n" +
-               "            \"example3@mail.ru\"\n" +
-               "        ],\n" +
-               "        \"id\": 1,\n" +
-               "        \"isAnonymous\": false,\n" +
-               "        \"name\": \"John\",\n" +
-               "        \"subscriptions\": [\n" +
-               "            4\n" +
-               "        ],\n" +
-               "        \"username\": \"user1\"\n" +
-               "    }\n" +
-               "}");
+       HashMap<String,String> profil = user.profil(email);
+       if (!profil.containsKey(email)) return Result.notFound();
+       List <String> followers = user.followers(email);
+       List <String> following = user.following(email);
+       List<Integer> subscriptions = user.subscriptions(email);
+       return Result.ok(new UserDetail(
+               profil.get("username"),
+               profil.get("about"),
+               profil.get("name"),
+               profil.get("email"),
+               new Integer(profil.get("id")),
+               Boolean.valueOf(profil.get("isAnonymous")),
+               followers.toArray(new String[followers.size()]),
+               following.toArray(new String[following.size()]),
+               subscriptions.toArray(new Integer[subscriptions.size()])));
    }
 
 
@@ -82,16 +81,15 @@ final public class UserController {
                "}");
    }
 
-    private  static final class CreateRequest {
-        private Boolean isAnonymous;
+    private  static class UserProfil {
         private String username;
         private String about;
         private String name;
         private String email;
         private Integer id;
-
+        private Boolean isAnonymous;
         @JsonCreator
-        private CreateRequest(@JsonProperty("username") String username,
+        private UserProfil(@JsonProperty("username") String username,
                               @JsonProperty("about") String about,
                               @JsonProperty("name") String name,
                               @JsonProperty("email") String email,
@@ -138,4 +136,64 @@ final public class UserController {
         }
     }
 
+    private static class UserDetail{
+        private String username;
+        private String about;
+        private String name;
+        private String email;
+        private Integer id;
+        private Boolean isAnonymous;
+        private String[] followers;
+        private String[] following;
+
+        private UserDetail(String username, String about, String name, String email, Integer id, Boolean isAnonymous, String[] followers, String[] following, Integer[] subscriptions) {
+            this.username = username;
+            this.about = about;
+            this.name = name;
+            this.email = email;
+            this.id = id;
+            this.isAnonymous = isAnonymous;
+            this.followers = followers;
+            this.following = following;
+            this.subscriptions = subscriptions;
+        }
+
+        private Integer[] subscriptions;
+
+        public String getAbout() {
+            return about;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String[] getFollowers() {
+            return followers;
+        }
+
+        public String[] getFollowing() {
+            return following;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public Boolean getAnonymous() {
+            return isAnonymous;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Integer[] getSubscriptions() {
+            return subscriptions;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+    }
 }
--- src/main/java/org/ebitbucket/services/User.java
+++ src/main/java/org/ebitbucket/services/User.java
@@ -5,38 +5,54 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 @Service
 public class User {
     private JdbcTemplate template;
-    private StringBuilder userCreated;
 
     public User(JdbcTemplate template) {
         this.template = template;
-        this.userCreated.
-                append("CREATE TABLE technopark.User\n").
-                append("(\n").
-                append("\tid INT PRIMARY KEY AUTO_INCREMENT,\n").
-                append("\temail VARCHAR(50) CHARACTER SET 'utf8' NOT NULL,\n").
-                append("\tname VARCHAR(50) CHARACTER SET 'utf8',\n").
-                append("\tusername VARCHAR(50) CHARACTER SET 'utf8',\n").
-                append("\tabout VARCHAR(50) CHARACTER SET 'utf8',\n").
-                append("\tisAnonymous BOOLEAN\n").
-                append(");\n").
-                append("CREATE UNIQUE INDEX User_email_uindex ON technopark.User (email);");
     }
 
     public int create(String email,String name,String username,String about,Boolean isAnonymous){
         try {
-            String sql = "INSERT INTO User(email, name, user_name, about, isAnonymous) VALUE(?,?,?,?,?)";
+            String sql = "INSERT INTO technopark.User(email, name, user_name, about, isAnonymous) VALUE(?,?,?,?,?)";
             template.update(sql, email, name, username, about, isAnonymous);
-            sql = "SELECT id FROM User WHERE email = ?";
+            sql = "SELECT id FROM technopark.User(email) VALUE (?)";
             return template.queryForObject(sql,Integer.class,email);
         }catch (DuplicateKeyException dk){
             return -1;
         }
     }
 
-    public StringBuilder getUserCreated() {
-        return userCreated;
+    public HashMap<String,String> profil(String email){
+        HashMap<String,String> result= new HashMap<>();
+        String sql = "SELECT * FROM technopark.User WHERE email = ?";
+        template.queryForMap(sql,result,email);
+        return result;
+    }
+
+    public List<String> following(String email){
+        List<String> result = new ArrayList<>();
+        String sql="SELECT email FROM technopark.User JOIN technopark.Following ON technopark.Following.parent=technopark.User.id AND technopark.Following.user = ?";
+        template.queryForList(sql,result,email);
+        return result;
+    }
+
+    public List<String> followers(String email){
+        List<String> result = new ArrayList<>();
+        String sql="SELECT email FROM technopark.User JOIN technopark.Followers ON technopark.Followers.parent=technopark.User.id AND technopark.Followers.user = ?";
+        template.queryForList(sql,result,email);
+        return result;
+    }
+
+    public List<Integer> subscriptions(String email){
+        List<Integer> result = new ArrayList<>();
+        String sql ="SELECT thread FROM technopark.Subscriptions WHERE technopark.Subscriptions.user=?";
+        template.queryForList(sql,result,email);
+        return result;
     }
 }
