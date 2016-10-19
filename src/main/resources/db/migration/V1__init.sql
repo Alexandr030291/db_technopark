3d101ee329046c9981551c9f7806c9ae53e171bd
Ivan Sopov <sopov.ivan@gmail.com>
2016-10-19 11:57:24 +0300

    Добавил систему миграций БД, база удобно создавалась сама удалил жесткую привязку к имени БД technopark, чтобы можно было запускать приложение на любом имени БД

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,56 @@
+CREATE TABLE User
+(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  email VARCHAR(50) NOT NULL,
+  name VARCHAR(50),
+  user_name VARCHAR(50),
+  about VARCHAR(50),
+  isAnonymous BOOLEAN
+);
+CREATE UNIQUE INDEX User_email_uindex ON User (email);
+
+CREATE TABLE Forum
+(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL,
+  short_name VARCHAR(50) NOT NULL,
+  user VARCHAR(50) NOT NULL,
+  CONSTRAINT Forum_User_email_fk FOREIGN KEY (user) REFERENCES User (email)
+);
+CREATE UNIQUE INDEX Forum_name_uindex ON Forum (name);
+CREATE UNIQUE INDEX Forum_short_name_uindex ON Forum (short_name);
+
+CREATE TABLE Post
+(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  date DATETIME,
+  thread INT,
+  message TEXT,
+  user VARCHAR(50) NOT NULL,
+  CONSTRAINT Post_User_email_fk FOREIGN KEY (user) REFERENCES User (email),
+  forum VARCHAR(50) NOT NULL,
+  CONSTRAINT Post_Forum_short_name_fk FOREIGN KEY (forum) REFERENCES Forum (short_name),
+
+  parent INT,
+  isApproved BOOLEAN,
+  isHighlighted BOOLEAN,
+  isEdited BOOLEAN,
+  isSpam BOOLEAN,
+  isDelete BOOLEAN
+);
+
+CREATE TABLE Thread
+(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  forum VARCHAR(50) NOT NULL,
+  CONSTRAINT Thread_Forum_short_name_fk FOREIGN KEY (forum) REFERENCES Forum (short_name),
+  title VARCHAR(50),
+  isClosed BOOLEAN,
+  user VARCHAR(50) NOT NULL,
+  CONSTRAINT Thread_User_email_fk FOREIGN KEY (user) REFERENCES User (email),
+  date DATETIME,
+  message TEXT,
+  slug VARCHAR(50),
+
+  isDelete BOOLEAN
+);
